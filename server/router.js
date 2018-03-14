2ec6c79d975655e0e9325bee00a5aaf5debd1b41
ametreniuc <metreniuk@gmail.com>
2018-03-14 23:28:16 +0200

    Add posting functionality

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const R = require('ramda')
 const { Entity } = require('./db')
 
 const router = express.Router()
@@ -9,11 +10,29 @@ router.route('/entity/:entityType')
 
     return Entity
       .find({ type: entityType })
-      .then(result => res
-        .status(200)
-        .json({ result }))
+      .then(result => res.status(200).json({ result }))
       .catch(err => res.status(404).json(err))
   })
+  .post((req, res) => {
+    const { body: { item } } = req
+    const entity = new Entity(item)
+
+    return entity.save()
+      .then(savedEntity => res.status(201).json(savedEntity))
+      .catch((err) => {
+        const errorMessages = Object
+          .values(err.errors)
+          .map(R.prop('message'))
+          .reduce(({ messages }, message) => ({
+            messages: [...messages, message],
+          }), { messages: [] })
+
+        return res.status(400).json(errorMessages)
+      })
+  })
+
+  // router.route('/entity/:entityType/:entityId')
+  //   .get()
 
 module.exports = {
   router,
